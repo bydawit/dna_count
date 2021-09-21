216fefad19169b90daae8682116e540eb9afb1bf
Dawit Gebeta <dawitgebeta@gmail.com>
2021-09-21 18:20:54 -0400

    Visualization

--- dna_app.py
+++ dna_app.py
@@ -9,7 +9,7 @@ st.image(image, use_column_width=True)
 st.write("""
 # DNA Nucleotide Count Web App
 
-This app counts the nucleotide composition if query DNA!
+This app counts the nucleotide composition of query DNA!
 
 ***
 """)
@@ -29,6 +29,50 @@ sequence = ''.join(sequence)
 st.write("""
 ***
 """)
+
 # DNA nucleotide count
 st.header('INPUT (DNA QUERY)')
 sequence
+
+st.header("OUTPUT (DNA Nucleotide Count)")
+
+st.subheader('1. Count')
+def dna_nucleotide_count(seq):
+    d = dict([
+            ('A', seq.count('A')),
+            ('T', seq.count('T')),
+            ('G', seq.count('G')),
+            ('C', seq.count('C'))
+    ])
+    return d
+
+
+X = dna_nucleotide_count(sequence)
+X_lable = list(X)
+X_values = list(X.values())
+X
+
+st.subheader('2. Description')
+st.write('Thre are ' + str(X['A']) + ' adenine (A)')
+st.write('Thre are ' + str(X['T']) + ' thymine (T)')
+st.write('Thre are ' + str(X['G']) + ' guanine (G)')
+st.write('Thre are ' + str(X['C']) + ' cytosine (C)')
+
+
+st.subheader('3. Dataframe')
+df = pd.DataFrame.from_dict(X, orient= 'index')
+df = df.rename({0: 'count'}, axis = 'columns')
+df.reset_index(inplace = True)
+df = df.rename(columns = {'index' : 'nucleotide'})
+st.write(df)
+
+
+st.subheader('4. Bar Chart')
+p = alt.Chart(df).mark_bar().encode(
+    x = 'nucleotide',
+    y = 'count'
+)
+p = p.properties(
+    width = alt.Step(80)
+)
+st.write(p)
